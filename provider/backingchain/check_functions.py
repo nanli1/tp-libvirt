fcb9cbe6f29fad4a17344af003a9ba576cce1d15
nanli <nanli@redhat.com>
2022-02-14 10:41:01 +0800

    add case for blockresize   add case for blockresize with b/mb/gb unit disk Signed-off-by: nanli <nanli@redhat.com>

--- provider/backingchain/check_functions.py
+++ provider/backingchain/check_functions.py
@@ -2,6 +2,7 @@ import logging
 import re
 
 from avocado.utils import process
+from virttest import utils_misc
 from virttest import libvirt_storage
 
 LOG = logging.getLogger('avocado.backingchain.checkfunction')
@@ -113,3 +114,80 @@ class Checkfunction(object):
             self.test.fail('qemu-img info output of backing chain '
                            'is not correct: %s' % img_info)
         return exist
+
+    def check_image_size(self, image_path):
+        """
+        Check image size with qemu-img command.
+
+        :param image_path: the path of image.
+        """
+        resize_value = self.params.get('new_size')
+        image_format = self.params.get('driver_type')
+
+        # Format expected image size
+        # Although kb should not be used, libvirt/virsh will accept it and
+        # consider it as a 1000 bytes, which caused issues for qed & qcow2
+        # since they expect a value evenly divisible by 512 (hence bz 1002813).
+        expected_size = 0
+        if "kb" in resize_value:
+            value = int(resize_value[:-2])
+            if image_format in ["qed", "qcow2"]:
+                # qcow2 and qed want a VIR_ROUND_UP value based on 512 byte
+                # sectors - hence this less than visually appealing formula
+                expected_size = (((value * 1000) + 512 - 1) // 512) * 512
+            else:
+                # Raw images...
+                # Ugh - there's some rather ugly looking math when kb
+                # (or mb, gb, tb, etc.) are used as the scale for the
+                # value to create an image. The blockresize for the
+                # running VM uses a qemu json call which differs from
+                # qemu-img would do - resulting in (to say the least)
+                # awkward sizes. We'll just have to make sure we don't
+                # deviates more than a sector.
+                expected_size = value * 1000
+        elif "kib" in resize_value:
+            value = int(resize_value[:-3])
+            expected_size = value * 1024
+        elif resize_value[-1] in "b":
+            expected_size = int(resize_value[:-1])
+        elif resize_value[-1] in "k":
+            value = int(resize_value[:-1])
+            expected_size = value * 1024
+        elif resize_value[-1] == "m":
+            value = int(resize_value[:-1])
+            expected_size = value * 1024 * 1024
+        elif resize_value[-1] == "g":
+            value = int(resize_value[:-1])
+            expected_size = value * 1024 * 1024 * 1024
+            cmd = "qemu-img info %s" % image_path
+            if libvirt_storage.check_qemu_image_lock_support():
+                cmd += " -U"
+            ret = process.run(cmd, allow_output_check='combined', shell=True)
+            status, output = (ret.exit_status, ret.stdout_text.strip())
+            value_return_by_qemu_img = re.\
+                search(r'virtual size:\s+(\d+(\.\d+)?)+\s?G', output).group(1)
+            if value != int(float(value_return_by_qemu_img)):
+                self.test.fail("initial image size in config is not "
+                               "equals to value returned by qemu-img info")
+        else:
+            self.test.error("Unknown scale value")
+
+        # Get current image size
+        image_info = utils_misc.get_image_info(image_path)
+        actual_size = int(image_info['vsize'])
+
+        LOG.info("The expected block size is %s bytes, "
+                 "the actual block size is %s bytes",
+                 expected_size, actual_size)
+
+        # Check the expected size and actualsize
+        # See comment above regarding Raw images
+        if image_format == "raw" and resize_value[-2] in "kb":
+            if abs(int(actual_size) - int(expected_size)) > 512:
+                self.test.fail("New raw blocksize set by blockresize do "
+                               "not match the expected value")
+        else:
+            if int(actual_size) != int(expected_size):
+                self.test.fail("New blocksize set by blockresize is "
+                               "different from actual size from "
+                               "'qemu-img info'")
